2550360658794002d71286a407c4758cfeec77cc
Agungthebesttop <agungthebesttop@gmail.com>
2021-11-07 00:28:29 +0700

    update main.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Agungthebesttop/synapseapp
+
+go 1.17
+
+require github.com/Agungthebesttop/synapsemodule v1.0.0
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Agungthebesttop/synapsemodule"
+
+func main() {
+
+	synapsemodule.PenjumlahanGreeting(15)
+}
